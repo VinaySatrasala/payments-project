7c8577a6a2a89a108772d74132fdea5d8540c2ba
VinaySatrasala <satrasalavinaykumar.01@gmail.com>
2024-12-11 23:57:13 +0530

    resolved store issue && added next-auth

---
+++ apps/user-app/src/app/lib/auth.ts
@@ -0,0 +1,78 @@
+import {db} from "@repo/db/client";
+import CredentialsProvider,{CredentialsConfig} from "next-auth/providers/credentials"
+import bcrypt from "bcrypt";
+import z from "zod"
+import { AuthOptions } from "next-auth";
+
+
+const CredentialsSchema = z.object({
+    phone: z.string().min(10).max(15),
+    password: z.string().min(6),
+});
+
+export const authOptions : AuthOptions = {
+    providers: [
+      CredentialsProvider({
+          name: 'Credentials',
+          credentials: {
+            phone: { label: "Phone number", type: "text", placeholder: "1231231231" },
+            password: { label: "Password", type: "password" }
+          },
+          // TODO: User credentials type from next-auth
+          async authorize(credentials:any) {
+            // Do zod validation, OTP validation here
+            const {success} = CredentialsSchema.safeParse(credentials);
+            if(!success){
+                throw new Error("Invalid credentials format")
+            }
+            const hashedPassword = await bcrypt.hash(credentials.password, 10);
+            const existingUser = await db.user.findFirst({
+                where: {
+                    number: credentials.phone
+                }
+            });
+
+            if (existingUser) {
+                const passwordValidation = await bcrypt.compare(credentials.password, existingUser.password);
+                if (passwordValidation) {
+                    return {
+                        id: existingUser.id.toString(),
+                        name: existingUser.name,
+                        email: existingUser.number
+                    }
+                }
+                return null;
+            }
+
+            try {
+                console.log("Reached")
+                const user = await db.user.create({
+                    data: {
+                        number: credentials.phone,
+                        password: hashedPassword
+                    }
+                });
+            
+                return {
+                    id: user.id.toString(),
+                    name: user.name,
+                    email: user.number
+                }
+            } catch(e) {
+                console.error(e);
+            }
+
+            return null
+          },
+        })
+    ],
+    secret: process.env.JWT_SECRET || "secret",
+    callbacks: {
+        // TODO: can u fix the type here? Using any is bad
+        async session({ token, session }:any) {
+            session.user.id = token.sub
+            return session
+        }
+    }
+  }
+ 
--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,1 +1,2 @@
-export * from "@prisma/client"
+import { PrismaClient } from "@prisma/client"
+export const db = new PrismaClient();
